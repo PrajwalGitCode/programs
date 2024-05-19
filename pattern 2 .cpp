7d2a376322a98eb51078bcf53b0060e4315bf435
Prajwal T <73645971+PrajwalGitCode@users.noreply.github.com>
2024-05-19 14:01:14 +0530

    Create pattern 2 .cpp

---
+++ pattern 2 .cpp
@@ -0,0 +1,43 @@
+//========== User's Code Starts Here ==========
+#include<iostream>
+using namespace std;
+
+void print_pattern();
+/*Function to print the pattern */
+
+void print_pattern()
+{
+        /* Function to print the pattern */
+        
+                for (int i = 5; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }    
+     
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }    
+ 
+
+ 
+ 
+ 
+ 
+ 
+ /* Dont change anything below. If changed click on reset. */
+
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    print_pattern();
+    return 0;
+}
+//========== User's Code Ends Here ==========
